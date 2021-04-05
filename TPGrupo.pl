453417f2bbf29825a9403eb81c5e24c5d25f48fc
melpereira7 <melanipereira98@gmail.com>
2021-04-05 14:53:55 +0100

    algumas correções

--- TPGrupo.pl
+++ TPGrupo.pl
@@ -31,7 +31,7 @@
 %centro_saude(Id,Nome,Morada,Telefone,Email).
 %staff: #Idstaff, #Idcentro, Nome, email ↝ { 𝕍, 𝔽 }
 %staff(Idstaff,Idcentro,Nome,Email).
-%vacinacao_covid: #Staf, #utente, Data, Vacina, Toma↝ { 𝕍, 𝔽 }
+%vacinacao_covid: #staff, #utente, Data, Vacina, Toma↝ { 𝕍, 𝔽 }
 %vacinacao_covid(Idstaff,Idutente,Data,Vacina,Toma).
 
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
@@ -43,53 +43,56 @@ utente(3,01234567890,'Igor Marcos',date(1989,8,20),'marcos.mendes@gmail.com',252
 utente(4,11111111111,'Rebeca Tavares Marques',date(2009,8,10),'maia.lara@gmail.com', 930643063, 'Avenida St. Valentim Fonseca, nº 3, 94º Dir. 9794-701 Quarteira', 'Estudante', [tuberculose] , 2).
 utente(5,09876543210,'Yasmin Sara Leite de Borges',date(1989,4,19),'dinis91@pereira.info',236082002, 'Avenida Leonardo Rodrigues, nº 2 3819-133 Aveiro','CEO Empresa de Marketing', [brucellosis,'gripe canina'],4).
 utente(6,66666666666,'José Aristoteles',date(1987,10,17),'exprimeiro@gov.pt',090909090,'Alameda','Engenheiro',[],1).
-utente(ID) :- utente(ID,_,_,_,_,_,_,_,_,_).
-%yasmin n foi vacinada
 
 centro_saude(1,'USFVida+','Vila Verde',253123456,'usfvida+@gmail.com').
 centro_saude(2,'UCSP Quarteira','Quarteira',244322111,'ucspquart@info.gmailcom').
 centro_saude(3,'USF infesta','São Mamede Infesta', 254254252,'centrosaude@gmail.com').
 centro_saude(4,'USF MOLICEIRO', 'Aveiro', 2512522523, 'aveirocsaude@gmail.com').
-centro_saude(ID) :- centro_saude(ID,_,_,_,_).
 
-staff(1,1,'Marria Silva','maria@gmail.com').
+staff(1,1,'Maria Silva','maria@gmail.com').
 staff(2,1,'Joana Guerra','joana@gmail.com').
 staff(3,1,'Miguel Santos', 'santosmiguel@gmail.com').
 staff(4,2,'Mario Sergio', 'sergiomario@gmail.com').
 staff(5,2,'Ricardo Salgado','salgadoricardo@bancoes.com').
 staff(6,3,'Joana Vasconcelos', 'arte@gmail.com').
 staff(7,4,'Pedro Granger', 'apresentador@rtp.pt').
-staff(ID) :- staff(ID,_,_,_).
 
 vacinacao_covid(1,1,date(2021,2,1),'Pfizer',1).
-vacinacao_covid(2,1,date(2021,2,5),'Pfizer',2).
-vacinacao_covid(1,2,date(2021,2,1),'Pfizer',1).
-vacinacao_covid(6,3,date(2021,3,10),'AstraZeneca',1).
-vacinacao_covid(4,4,date(2021,3,11),'Pfizer',1).
-vacinacao_covid(5,4,date(2021,3,26),'Pfizer',2).
-vacinacao_covid(6,3,date(2021,6,10),'AstraZeneca',2).
-vacinacao_covid(7,6,date(2021,2,5),'AstraZeneca',2).
+vacinacao_covid(2,1,date(2021,2,16),'Pfizer',2).
+vacinacao_covid(1,2,date(2021,2,28),'Pfizer',1).                %vacinada indevidamente e falta segunda toma
+vacinacao_covid(6,3,date(2021,3,20),'AstraZeneca',1).
+vacinacao_covid(4,4,date(2021,3,17),'Pfizer',1).
+vacinacao_covid(5,4,date(2021,4,1),'Pfizer',2).
+vacinacao_covid(6,3,date(2021,4,10),'AstraZeneca',2).
+vacinacao_covid(7,6,date(2021,2,5),'AstraZeneca',1).            %vacinada indevidamente
+
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% Predicados para simplificar a verificação da existência de um certo utente, centro, staff ou vacinação
+
+utente(ID) :- utente(ID,_,_,_,_,_,_,_,_,_).
+centro_saude(ID) :- centro_saude(ID,_,_,_,_).
+staff(ID) :- staff(ID,_,_,_).
 vacinacao_covid(Idstaff,Idutente) :- vacinacao_covid(Idstaff,Idutente,_,_,_).
+
 
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
 % Permitir a definição de fases de vacinação, definindo critérios de inclusão de utentes nas diferentes fases (e.g., doenças crónicas, idade, profissão);
 
-fase(1,Id,date(2021,2,1)) :- idade(Id,I), I>80.
-faseLista(1,L,date(2021,2,1)) :- solucoes(Id,(idade(Id,I),I>=80),L).
+fase(1,Id,date(2021,2,1)) :- idade(Id,I), I>=80.
+faseLista(1,L,date(2021,2,1)) :- solucoes(Id,fase(1,Id,date(2021,2,1)),L).
 
-fase(2,Id,date(2021,3,29)) :- utente(Id,_,_,_,_,_,_,_,DoencasCronicas,_), nao(vazia(DoencasCronicas)).
-faseLista(2,L,date(2021,3,29)) :- solucoes(Id,(utente(Id,_,_,_,_,_,_,_,DoencasCronicas,_), nao(vazia(DoencasCronicas))),L).
+fase(2,Id,date(2021,3,16)) :- utente(Id,_,_,_,_,_,_,_,DoencasCronicas,_), nao(vazia(DoencasCronicas)).
+faseLista(2,L,date(2021,3,16)) :- solucoes(Id,fase(2,Id,date(2021,3,16)),L).
 
 fase(3,Id,date(2021,4,29)) :- 
     utente(Id,_,_,_,_,_,_,Profissao,_,_),
     pertence(Profissao,['Médica','Médico','Enfermeira','Enfermeiro','Auxiliar de Saúde','Professora','Professor']).
-faseLista(3,L,date(2021,4,29)) :-
-    solucoes(Id,(utente(Id,_,_,_,_,_,_,Profissao,_,_),
-                pertence(Profissao,['Médica','Médico','Enfermeira','Enfermeiro','Auxiliar de Saúde','Professora','Professor'])),L).
+faseLista(3,L,date(2021,4,29)) :- solucoes(Id,fase(3,Id,date(2021,4,29)),L).
 
-fase(4,Id,date(2021,6,12)) :- nao(fase(1,Id,_)), nao(fase(2,Id,_)), nao(fase(3,Id,_)).
+fase(4,Id,date(2021,6,12)) :- utente(Id), nao(fase(1,Id,_)), nao(fase(2,Id,_)), nao(fase(3,Id,_)).
+faseLista(4,L,date(2021,6,12)) :- solucoes(Id,fase(4,Id,date(2021,6,12)),L).
 
-%Alterar para subtrair dia e mes
 idade(Id,I) :- utente(Id,_,_,Data_Nasc,_,_,_,_,_,_), date(DataAtual), date_interval(DataAtual,Data_Nasc, I years).
 vazia([]).
 
@@ -188,10 +191,6 @@ removerVacinacao(Idstaff,Idutente,Data,Vacina,Toma) :-
 
 
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
-% exemplos que criei só para testes
-
-excecao(vacinada('Maria do Olival')).
-
 % Não pode haver mais que um utente com os mesmos dados
 +utente(Idutente,NISS,Nome,Data_Nasc,Email,Telefone,Morada,Profissao,DoencasCronicas,Idcentro) :: 
                 (solucoes(Idutente,utente(Idutente),S),
