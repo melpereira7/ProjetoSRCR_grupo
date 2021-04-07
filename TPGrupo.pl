02df379123a5ca902e921f505aedd4a2f1ddbd61
Duarte Vilar <48862635+mishlawi@users.noreply.github.com>
2021-04-07 12:13:22 +0100

    Add files via upload

--- TPGrupo.pl
+++ TPGrupo.pl
@@ -10,6 +10,7 @@
 :- set_prolog_flag(w:unknown,fail).
 
 :- use_module(library(date_time)).
+:- use_module(library(lists)).
 
 %para dar up do menu
 :- consult(menu).
@@ -39,17 +40,27 @@
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
 % Base de conhecimento (com exemplos arbitrários)
 
-utente(1,211111111,'Ana Murciellago',date(1930,1,1),'ana@gmail.com',911234567,'Braga','Médica',[],1).
-utente(2,221111111,'Maria do Olival',date(2001,11,5), 'maria@gmail.com',936484263 ,'Viana do Castelo', 'Professora', [asma], 1).
-utente(3,01234567890,'Igor Marcos',date(1989,8,20),'marcos.mendes@gmail.com',252252252,'R. Amaral 8499-852 São Mamede de Infesta','Agricultor', [hiv,hepatite], 3).
-utente(4,11111111111,'Rebeca Tavares Marques',date(2009,8,10),'maia.lara@gmail.com', 930643063, 'Avenida St. Valentim Fonseca, nº 3, 94º Dir. 9794-701 Quarteira', 'Estudante', [tuberculose] , 2).
-utente(5,09876543210,'Yasmin Sara Leite de Borges',date(1989,4,19),'dinis91@pereira.info',236082002, 'Avenida Leonardo Rodrigues, nº 2 3819-133 Aveiro','CEO Empresa de Marketing', [brucellosis,'gripe canina'],4).
-utente(6,66666666666,'José Aristoteles',date(1987,10,17),'exprimeiro@gov.pt',090909090,'Alameda','Engenheiro',[],1).
+utente(1,21111111112,'Ana Murciellago',date(1930,1,1),'ana@gmail.com',911234567,'Braga','Médica',[],1).
+utente(2,22111111112,'Maria do Olival',date(2001,11,5), 'maria@gmail.com',936484263 ,'Viana do Castelo', 'Professora', ['asma'], 1).
+utente(3,01234567890,'Igor Marcos',date(1989,8,20),'marcos.mendes@gmail.com',252252252,'R. Amaral 8499-852 São Mamede de Infesta','Agricultor', ['hiv','hepatite'], 3).
+utente(4,11111111111,'Rebeca Tavares Marques',date(2009,8,10),'maia.lara@gmail.com', 930643063, 'Avenida St. Valentim Fonseca, nº 3, 94º Dir. 9794-701 Quarteira', 'Estudante', ['tuberculose'] , 2).
+utente(5,09876543210,'Yasmin Sara Leite de Borges',date(1989,4,19),'dinis91@pereira.info',236082002, 'Avenida Leonardo Rodrigues, nº 2 3819-133 Aveiro','CEO Empresa de Marketing', ['brucellosis','gripe canina'],4).
+utente(6,66666666666,'José Aristoteles',date(1967,1,29),'exprimeiro@gov.pt',090909090,'Alameda','Engenheiro',[],9).
+utente(7,12345652858,'Custódio Borges',date(1987,4,15),'custodio@sapo.pt',251252254,'Rua Esteves Vasto 23 Santo Tirso','Calceiteiro',['Hipertensão','Colesterol'],7).
+utente(8,15615956260,'Francisco Carriço',date(1977,4,17),'carriçochico@yahoo.com',255256258,'Rua Professor Batata Aracena','Advogado',['Diabetes', 'Artrite'],8).
+utente(9,30861910266,'Jacinta Barboza',date(1982,2,13),'jacibar@gmail.com',256257269,'Rua do Rei de Fafe, Fafe','Empresário',['Alzheimer', 'Doença crónica dos rins'],10).
 
-centro_saude(1,'USFVida+','Vila Verde',253123456,'usfvida+@gmail.com').
+centro_saude(1,'USFVida+','Vila Verde',253123456,'usfvida+@hotmail.com').
 centro_saude(2,'UCSP Quarteira','Quarteira',244322111,'ucspquart@info.gmailcom').
 centro_saude(3,'USF infesta','São Mamede Infesta', 254254252,'centrosaude@gmail.com').
-centro_saude(4,'USF MOLICEIRO', 'Aveiro', 2512522523, 'aveirocsaude@gmail.com').
+centro_saude(4,'USF MOLICEIRO', 'Aveiro', 2512522523, 'aveirocsaude@saude.com').
+centro_saude(5,'CS Penha de Franhça', 'Rua Luís Pinto Moitinho,5 Lisboa', 224322123, 'cspenhafranca@gmail.com').
+centro_saude(6,'Centro de Saude Caldas da Rainha', 'Rua do Centro de Saude, Caldas da Rainha', 2412422423,'caldasofthequeen@gmail.com').
+centro_saude(7,'Centro Saúde Lousã', 'Alameda Juiz Conselheiro Nunes Ribeiro, Vilarinho',232123123,'vilarinholsa@minsaude.com').
+centro_saude(8,'Centro de Saúde S.Pedro do Sul','Avenida da Ponte, São Pedro do Sul',224123412, 'saintpetersouth@minsaude.com').
+centro_saude(9,'Centro Saúde Ovar', 'Rua Dr. Francisco Zagalo s/n, OVAR', 245245245,'ovarhashealth@minsaude.com').
+centro_saude(10,'Centro de Saude Almodovar','Rua Professor Dr Fernando Padua, ALMODOVAR', 263264265,'almodovaralmodovar@minsaude.com').
+
 
 staff(1,1,'Maria Silva','maria@gmail.com').
 staff(2,1,'Joana Guerra','joana@gmail.com').
@@ -58,6 +69,14 @@ staff(4,2,'Mario Sergio', 'sergiomario@gmail.com').
 staff(5,2,'Ricardo Salgado','salgadoricardo@bancoes.com').
 staff(6,3,'Joana Vasconcelos', 'arte@gmail.com').
 staff(7,4,'Pedro Granger', 'apresentador@rtp.pt').
+staff(8,5,'Luis Magalhaes', 'luismagalean@saudept.pt').
+staff(9,6,' Mario Carvalhal', 'carvalhal@braga.pt').
+staff(10,7,'Sergio Pedro', 'pedrosergio@cso.pt').
+staff(11,8,'Ana Silva', 'anasilva123@sapo.pt').
+staff(12,8,'Ricardo Tomé', 'riccardoootome@yahoo.com').
+staff(13,9,'Tiago Xavier', 'xavy99gem@riot.com').
+staff(14,7,'Miguel Tavares', 'miguelsousatavares@tvi.pt').
+staff(15,10,'Sophia Anderson', 'sophiamellobryner@denmark.de').
 
 vacinacao_covid(1,1,date(2021,2,1),'Pfizer',1).
 vacinacao_covid(2,1,date(2021,2,16),'Pfizer',2).
@@ -68,6 +87,35 @@ vacinacao_covid(5,4,date(2021,4,1),'Pfizer',2).
 vacinacao_covid(6,3,date(2021,4,10),'AstraZeneca',2).
 vacinacao_covid(7,6,date(2021,2,5),'AstraZeneca',1).            %vacinada indevidamente
 
+%medico_familia : #idmedico,nome,email,idcentro ↝ { 𝕍, 𝔽 }
+medico_familia(1,'Carlos Estevão','carlitos@gmail.com',1).
+medico_familia(2,'Maria Silvana','silvana@gmail.com',3).
+medico_familia(3,'Rogério Magalhães','magalhaes@gmail.com',2).
+medico_familia(4,'Luis Ricciardi','ricciardi76@gmail.com',4).
+medico_familia(5,'Joana Cunha','joanaCunha@gmail.com',5).
+medico_familia(6,'Maria Suzete','suzetemaria@gmail.com',6).
+medico_familia(7,'José Carlos Saraiva','zecarlos@gmail.com',7).
+medico_familia(8,'Manuel Saramago','sarmanu@gmail.com',8).
+medico_familia(9,'Ana Costa Silva','anacostafilomena@hotmail.com',9).
+medico_familia(10,'Luisa Sobral','sobraleurovision@almodovarcs.pt',10).
+medico_familia(11,'Salvador Sentido','semsentid@gmail.com',9).
+medico_familia(12,'Manuel Rodrigues','sarmanu@sapo.pt',8).
+
+
+% consulta:#IdConsult,#IdUt,#idmedico,#idcentro,Descrição,Custo,,Data ↝ { 𝕍, 𝔽 }
+consulta(1,1,2,3,'Consulta de rotina',15.00,(2021,4,21)).
+consulta(2,2,1,1,'Dores pós vacinação',7.50,(2021,4,11)).
+consulta(3,2,3,2,'Relatorio medicação HIV',15.00,(2021,3,7)).
+consulta(4,4,5,5,'Formulação de baixa laboral',70.00,(2021,7,1)).
+consulta(5,4,4,4,'Avaliacao de teste psicotécnico',20.70,(2021,5,11)).
+consulta(6,5,7,7,'Consulta de rotina',15.00,(2021,11,3)).
+consulta(7,7,8,9,'Consulta de rotina',15.00,(2021,7,6)).
+consulta(8,8,6,6,'Verificação exames diabéticos',15.00,(2021,2,13)).
+consulta(9,9,11,9,'Consulta de rotina',15.00,(2021,1,11)).
+consulta(10,9,11,9,'Marcação de exames rotineiros',5.00,(2021,5,10)).
+
+
+
 
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
 % Predicados para simplificar a verificação da existência de um certo utente, centro, staff ou vacinação
@@ -76,6 +124,8 @@ utente(ID) :- utente(ID,_,_,_,_,_,_,_,_,_).
 centro_saude(ID) :- centro_saude(ID,_,_,_,_).
 staff(ID) :- staff(ID,_,_,_).
 vacinacao_covid(Idstaff,Idutente) :- vacinacao_covid(Idstaff,Idutente,_,_,_).
+medico_familia(IdMedico):-medico_familia(IdMedico,_,_,_).
+consulta(IdUtente,IdMedico) :- consulta(_,IdUtente,IdMedico,_,_,_,_).
 
 
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
@@ -174,6 +224,15 @@ registarStaff(Idstaff,Idcentro,Nome,Email) :-
 registarVacinacao(Idstaff,Idutente,Data,Vacina,Toma) :-
     evolucao(vacinacao_covid(Idstaff,Idutente,Data,Vacina,Toma)).
 
+% medico_familia
+registarMedico(IdMedico,Nome,Email,IdCentro):-
+    evolucao(medico_familia(Idstaff,Nome,Email,IdCentro)).
+
+
+%consulta
+registarConsulta(IdConsulta,IdUtente,IdMedico,IdCentro,Descricao,Custo,Data):-
+    evolucao(consulta(IdConsulta,IdUtente,IdMedico,IdCentro,Descricao,Custo,Data)).
+
 
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
 %---- Remoções
@@ -200,8 +259,24 @@ removerStaff(Idstaff) :-
 removerVacinacao(Idstaff,Idutente,Data,Vacina,Toma) :-
     involucao(vacinacao_covid(Idstaff,Idutente,Data,Vacina,Toma)).
 
+% Medico
+removerMedico(IdMedico):-
+    medico_familia(IdMedico,Nome,Email,IdCentro),
+    involucao(medico_familia(IdMedico,Nome,Email,IdCentro)).
 
-%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+%consulta
+
+removerConsulta(IdConsulta,IdUtente,IdMedico,IdCentro,Descricao,Custo,Data):-
+    involucao(consulta(IdConsulta,IdUtente,IdMedico,IdCentro,Descricao,Custo,Data)).
+
+
+%--- Extras
+
+
+%custo total consultas por utente
+
+
+
 % Não pode haver mais que um utente com o mesmo identificador
 +utente(Idutente,NISS,Nome,Data_Nasc,Email,Telefone,Morada,Profissao,DoencasCronicas,Idcentro) :: 
                 (solucoes(Idutente,utente(Idutente),S),
@@ -244,6 +319,7 @@ removerVacinacao(Idstaff,Idutente,Data,Vacina,Toma) :-
                  comprimento(S,N),
                  N==1).
 
+
 % Não pode haver staff de um centro de saúde que não é conhecido pelo sistema
 +staff(Idstaff,Idcentro,Nome,Email) ::
                 (solucoes(Idcentro,centro_saude(Idcentro),S),
@@ -259,6 +335,18 @@ removerVacinacao(Idstaff,Idutente,Data,Vacina,Toma) :-
                 (solucoes(Idutente,(utente(Idutente,_,_,_,_,_,_,_,_,Idcentro)),S),
                 comprimento(S,N),
                 N==0).
+
+% Não pode haver mais que um medico com o mesmo identificador
++medico_familia(Idmedico,Nome,Email,IdCentro) ::
+                (solucoes(Idmedico,medico_familia(Idmedico),S),
+                 comprimento(S,N),
+                 N==1).
+
+% Não pode haver uma consulta feita por um medico a um utente que não sejam conhecidos pelo sistema
++consulta(IdConsult,Idutente,Idmedico,IdCentro,Descricao,Custo,Data) :: 
+                (solucoes((Idmedico,Idutente),(medico_familia(Idmedico),utente(Idutente)),S),
+                 comprimento(S,N),
+                N==1).
 
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
 % Extensao do predicado que permite a evolucao do conhecimento
--- menu.pl
+++ menu.pl
@@ -15,13 +15,17 @@ menu:- write('\n'),
        write('2.Registar Centro de Saúde \n'),
        write('3.Registar Elemento do Staff \n'),
        write('4.Registar Vacinação \n'),
+       write('5.Registar Medico de Familia \n'),
+       write('6.Registar Consulta \n'),
        write('\n'),
        write('-----------Delete-----------\n'),
-       write('5.Apagar Utente \n'),
-       write('6.Apagar Centro de Saúde \n'),
-       write('7.Apagar Elemento do Staff \n'),
-       write('8.Apagar Vacinação \n'),
+       write('7.Apagar Utente \n'),
+       write('8.Apagar Centro de Saúde \n'),
+       write('9.Apagar Elemento do Staff \n'),
+       write('10.Apagar Vacinação \n'),
        write('\n'),
+       write('-----------Extras-----------\n'),
+       write('11.Custo total consultas por utente\n'),
        write('0.Sair \n'),
        read(Option),
        executar(Option).
@@ -30,10 +34,14 @@ executar(Option):-Option =:=1,addUtente,menu;
                   Option =:=2,addCentro,menu;
                   Option =:=3,addStaff,menu;
                   Option =:=4,addVacinacao,menu;
-                  Option =:=5,deleteUtente,menu;
-                  Option =:=6,deleteCentro,menu;
-                  Option =:=7,deleteStaff,menu;
-                  Option =:=8,deleteVacinacao,menu;
+                  Option =:=5,addMedico,menu;
+                  Option =:=6,addConsulta,menu;
+                  Option =:=7,deleteUtente,menu;
+                  Option =:=8,deleteCentro,menu;
+                  Option =:=9,deleteStaff,menu;
+                  Option =:=10,deleteVacinacao,menu;
+                  Option =:=11,deleteMedico,menu;
+                  Option =:=10,deleteConsulta,menu;
                   Option =:=0,true,write('Goodbye.').
 
 /*-------------------------------- INSERÇÕES -------------------------------- */
@@ -60,7 +68,7 @@ addCentro:-write('Id Centro de Saúde: '),read(Id),
 addStaff:-write('Id Staff: '),read(IdStaff),
                  write('Id Centro: '),read(IdCentro),
                  write('Nome: '),read(Nome),
-                 write('Email'),read(Email),
+                 write('Email: '),read(Email),
                  registarStaff(IdConsult,IdUt,IdPrestador,IdServ,Descricao,Custo,Data).
 
 addVacinacao:-write('Id Staff: '),read(IdStaff),
@@ -70,36 +78,36 @@ addVacinacao:-write('Id Staff: '),read(IdStaff),
                    write('Toma: '),read(Toma),
                    registarVacinacao(Idstaff,Idutente,Data,Vacina,Toma).
 
+addMedico:-write('Id médico:  '), read(IdMedico),
+                 write('Nome: '),read(Nome),
+                 write('Email: '),read(Email),
+                 write('Id Centro: '),read(IdCentro),
+                 registarMedico(IdMedico,Nome,Email,IdCentro).
+
+
+addConsulta:-write('Id Consulta:  '),read(IdConsulta),
+                  write('Id utente:  '),read(Idutente),
+                  write('Id Médico:  '),read(IdMedico),
+                  write('Id Centro.  '),read(IdCentro),
+                  write('Descrição do motivo da consulta: '), read(Descricao),
+                  write('Custo da consulta:  '), read(Custo),
+                  write('Data da consulta:  '), read(Data),
+                  registarConsulta(IdConsulta,IdUtente,IdMedico,IdCentro,Descricao,Custo,Data).
+
+
 
 /*------------------------------- REMOÇÕES ---------------------------------- */
 
 deleteUtente:-   write('Id Utente: '),read(Idutente),
-                 write('NSS: '),read(NISS),
-                 write('Nome: '),read(Nome),
-                 write('Data de Nascimento: '),read(Data_Nasc),
-                 write('Email: '),read(Email),
-                 write('Telefone: '),read(Telefone),
-                 write('Morada: '),read(Morada),
-                 write('Profissao: '),read(Profissao),
-                 write('Lista de doenças crónicas: '),read(DoencasCronicas),
-                 write('Id Centro de Saude: '),read(IdCentro),
-                 removerUtente(Idutente,NISS,Nome,Data_Nasc,Email,Telefone,Morada,Profissao,DoencasCronicas,Idcentro).
+                 removerUtente(Idutente).
 
 
-deleteCentro :-  write('Id Centro de Saúde: '),read(Id),
-                 write('Nome: '),read(Nome),
-                 write('Morada'),read(Morada),
-                 write('Telefone: '),read(Telefone),
-                 write('E-mail: '),read(Email),
-                 removerCentro(IdServ,Especialidade,Instituicao,Cidade).
+deleteCentro :-  write('Id Centro de Saúde: '),read(IdServ),
+                 removerCentro(IdServ).
 
 
 deleteStaff:- write('Id Staff: '),read(IdStaff),
-              write('Id Centro: '),read(IdCentro),
-              write('Nome: '), read(Nome),
-              write('Email: '),read(Email),
-              removerStaff(Idstaff,Idcentro,Nome,Email).
-
+              removerStaff(IdStaff).
 
 deleteVacinacao:-  write('Id Staff: '),read(IdStaff),
                    write('Id Utente: '),read(Idutente),
@@ -108,3 +116,16 @@ deleteVacinacao:-  write('Id Staff: '),read(IdStaff),
                    write('Toma: '),read(Toma),
                    removerVacinacao(Idstaff,Idutente,Data,Vacina,Toma).
 
+
+deleteMedico:-write('Id médico:  '), read(IdMedico),
+              removerMedico(IdMedico).
+
+
+deleteConsulta:-write('Id Consulta:  '),read(IdConsulta),
+                  write('Id utente:  '),read(Idutente),
+                  write('Id Médico:  '),read(IdMedico),
+                  write('Id Centro.  '),read(IdCentro),
+                  write('Descrição do motivo da consulta: '), read(Descricao),
+                  write('Custo da consulta:  '), read(Custo),
+                  write('Data da consulta:  '), read(Data),
+                  removerConsulta(IdConsulta,IdUtente,IdMedico,IdCentro,Descricao,Custo,Data).
